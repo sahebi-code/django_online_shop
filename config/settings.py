8ccc4b194b1ad46ddd25f1c0fb09d35b043a694d
Mohammad Sahebi <msahebi.me@gmail.com>
2026-01-08 00:30:44 +0330

    add static files

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from environs import Env
+import os
 
 # For environment variables
 env = Env()
@@ -151,6 +152,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
 
 # accounts config
 AUTH_USER_MODEL = "accounts.CustomUser"
