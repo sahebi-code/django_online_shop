18f198a246cc854c679d9abd4450b8f3253e7fde
Mohammad Sahebi <msahebi.me@gmail.com>
2026-01-07 17:33:58 +0330

    change some accounts templates

--- config/settings.py
+++ config/settings.py
@@ -127,7 +127,12 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "your_account@gmail.com"
+EMAIL_HOST_PASSWORD = "your account's password"
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
