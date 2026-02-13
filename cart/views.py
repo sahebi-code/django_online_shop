6ee818c9b89d9ba3759a8910a94b6c0d47946d9b
Mohammad Sahebi <msahebi.me@gmail.com>
2026-02-13 13:09:31 +0330

    implement order process

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.http import require_POST
 from products.models import Product
+from django.contrib import messages
+from django.utils.translation import gettext as _
 
 from .cart import Cart
 from .forms import AddToCartProductForm
@@ -36,3 +38,14 @@ def remove_from_cart(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
     return redirect('cart:cart_detail')
+
+def clear_cart(request):
+    cart = Cart(request)
+
+    if len(cart):
+        cart.clear()
+        messages.success(request, _('All products have been successfully removed from your cart.'))
+    else:
+        messages.warning(request, _('Your cart is already empty.'))
+
+    return redirect('product_list')
--- config/settings.py
+++ config/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     "products.apps.ProductsConfig",
     "cart.apps.CartConfig",
     "persian_translate.apps.PersianTranslateConfig",
+    "orders.apps.OrdersConfig",
 
     # third party apps
     'rosetta',
